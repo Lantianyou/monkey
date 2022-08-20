9fb9a02c8c8cff51d54b07e8a9e9871295a13a85
兰天游 <dtlantianyou@gmail.com>
2022-08-20 11:44:57 +0800

    feat: read identifier

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module tianyou.net/monkey
+module monkey
 
-go 1.18
+go 1.19
--- lexer/lexter_test.go
+++ lexer/lexter_test.go
@@ -1,39 +1,67 @@
 package lexer
 
 import (
+	"monkey/token"
 	"testing"
-
-	"tianyou.net/monkey/token"
 )
 
-func TestNextToken(t *testing.T) {
-	input := "=+(){},;let five = 5;let ten = 10;let add = fn(x, y) {x + y;};let result = add(five, ten);"
-
+func TestNextToken2(t *testing.T) {
+	input := `let five=5;
+let ten =10;
+let add = fn(x, y){
+  x+y;
+};
+let result = add(five, ten);
+`
 	tests := []struct {
 		expectedType    token.TokenType
 		expectedLiteral string
 	}{
+		{token.LET, "let"},
+		{token.IDENT, "five"},
 		{token.ASSIGN, "="},
-		{token.PLUS, "+"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "ten"},
+		{token.ASSIGN, "="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "add"},
+		{token.ASSIGN, "="},
+		{token.FUNCTION, "fn"},
 		{token.LPAREN, "("},
+		{token.IDENT, "x"},
+		{token.COMMA, ","},
+		{token.IDENT, "y"},
 		{token.RPAREN, ")"},
 		{token.LBRACE, "{"},
-		{token.RBRACE, "}"},
-		{token.COMMA, ","},
+		{token.IDENT, "x"},
+		{token.PLUS, "+"},
+		{token.IDENT, "y"},
 		{token.SEMICOLON, ";"},
-		{token.EOF, ""},
+		{token.RBRACE, "}"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "result"},
+		{token.ASSIGN, "="},
+		{token.IDENT, "add"},
+		{token.LPAREN, "("},
+		{token.IDENT, "five"},
+		{token.COMMA, ","},
+		{token.IDENT, "ten"},
+		{token.RPAREN, ")"},
+		{token.SEMICOLON, ";"},
 	}
-
 	l := New(input)
-
 	for i, tt := range tests {
 		tok := l.NextToken()
-
 		if tok.Type != tt.expectedType {
-			t.Fatalf("types wrong")
+			t.Fatalf("tests[%d] - tokenType wrong, expected=%q, got=%q", i, tt.expectedType, tok.Type)
 		}
 		if tok.Literal != tt.expectedLiteral {
-			t.Fatalf("literal wrong")
+			t.Fatalf("tests[%d] - Literal wrong, expected=%q, got=%q", i, tt.expectedLiteral, tok.Literal)
 		}
 	}
 }
