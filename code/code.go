1bfe0f975066eeb53072c2bbd8ca293e5249e831
兰天游 <dtlantianyou@gmail.com>
2022-08-24 13:23:25 +0800

    feat: add definition for debug

--- code/code.go
+++ code/code.go
@@ -3,3 +3,24 @@ package code
 type Instructions []byte
 
 type Opcode byte
+
+const (
+	OpConstant Opcode = iota
+)
+
+type Definition struct {
+	Name string
+	OperandWidths []int
+}
+
+var definitions = map[Opcode]*Definition {
+	OpConstant: {"OpConstant", []int{2}}
+}
+
+func Lookup(op byte) (*Definition, error) {
+	def, ok := definitions[op]
+	if !ok {
+		return nil, fmt.Errorf("opcode %d undefined", op``)
+	}
+	return def, nil
+}
